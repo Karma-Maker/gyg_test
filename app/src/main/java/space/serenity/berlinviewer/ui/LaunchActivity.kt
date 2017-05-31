4bde80a9ba564fee737a20897c7fb6bd29436410
Karma Maker <karmamaker.dev@gmail.com>
2017-05-31 21:16:19 +0300

    UI changes

--- app/src/main/java/space/serenity/berlinviewer/ui/LaunchActivity.kt
+++ app/src/main/java/space/serenity/berlinviewer/ui/LaunchActivity.kt
@@ -8,6 +8,9 @@ import android.support.v7.widget.LinearLayoutManager
 import space.serenity.berlinviewer.R
 import kotlinx.android.synthetic.main.activity_launch.*
 import space.serenity.berlinviewer.ui.adapters.ReviewsAdapter
+import android.support.v7.widget.DividerItemDecoration
+
+
 
 
 class LaunchActivity : AppCompatActivity() {
@@ -15,10 +18,15 @@ class LaunchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch)
+
+        // list
         list.layoutManager = LinearLayoutManager(this)
-        val adapter = ReviewsAdapter();
+        list.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+        val adapter = ReviewsAdapter()
         list.adapter = adapter
         adapter.deleteMeInit()
+
+        // fab
         val fab = findViewById(R.id.fab) as FloatingActionButton
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
